6574b6a6bb1c1b3a58b17e6bb9cafd041f5fcd76
kodebae <heykarmendurbin@gmail.com>
2024-08-18 23:43:48 -0400

    bug: form submission and implementation needs to be debugged

--- app-kodebae/src/App.jsx
+++ app-kodebae/src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css';
-import Hero from './sections/Hero/Hero';
-import Projects from './sections/Projects/Projects';
+import './App.css'
+import Hero from './sections/Hero/Hero'
+import Projects from './sections/Projects/Projects'
+import Contact from './sections/Contact/Contact'
 
 function App() {
   return (
     <>
       <Hero />
       <Projects />
+      <Contact />
     </>
   );
 }
 
-export default App;
+export default App
