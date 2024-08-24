b7537ee30218c846132202d496bed260796bdb80
kodebae <heykarmendurbin@gmail.com>
2024-08-24 00:43:59 -0400

    add: implementing gh actions

--- app-kodebae/vite.config.js
+++ app-kodebae/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Using the base option to specify the deployment path
 export default defineConfig({
   plugins: [react()],
+  base: '/kodebae-app/',
 })
