9fe1b54a609b3f48eb3b61514a2fca064e9000b6
kodebae <heykarmendurbin@gmail.com>
2024-08-24 09:28:44 -0400

    update: gh actions update

--- app-kodebae/vite.config.js
+++ app-kodebae/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Using the base option to specify the deployment path
 export default defineConfig({
   plugins: [react()],
-  base: '/kodebae-app/',
+  base: '/kodebae-app/app-kodebae/',
 })
